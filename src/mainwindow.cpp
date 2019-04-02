69eb5011df70dbc9bb9db2fd54140cdf9f6b1d6e
Alan Garny <agarny@hellix.com>
2019-04-03 12:51:27 +1300

    Some minor cleaning up.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -52,10 +52,10 @@ void ClickableLabel::mousePressEvent(QMouseEvent *pEvent)
 
 //==============================================================================
 
-static const auto SettingsRadio    = QStringLiteral("Radio");
-static const auto SettingsVolume   = QStringLiteral("Volume");
-static const auto FranceInfoRadio  = QStringLiteral("https://direct.franceinfo.fr/live/franceinfo-midfi.mp3");
-static const auto NostalgieRadio   = QStringLiteral("https://cdn.nrjaudio.fm/audio1/fr/30601/mp3_128.mp3");
+static const auto SettingsRadio   = QStringLiteral("Radio");
+static const auto SettingsVolume  = QStringLiteral("Volume");
+static const auto FranceInfoRadio = QStringLiteral("https://direct.franceinfo.fr/live/franceinfo-midfi.mp3");
+static const auto NostalgieRadio  = QStringLiteral("https://cdn.nrjaudio.fm/audio1/fr/30601/mp3_128.mp3");
 
 //==============================================================================
 
